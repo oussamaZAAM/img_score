7cd1410ce0a37b50d9bb8a9d89ba7e3fd190c6e0
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-05 22:30:29 +0000

    A function that resizes the image "squarized" before to return the same image (without distortion) with dimensions = (32, 32) to adapt the trained model.

---
+++ resize.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+def image_resize(img, size):
+    h, w = img.shape[:2]
+    c = img.shape[2] if len(img.shape)>2 else 1
+
+    if h == w: 
+        return cv2.resize(img, size, cv2.INTER_AREA)
+
+    dif = h if h > w else w
+
+    interpolation = cv2.INTER_AREA if dif > (size[0]+size[1])//2 else cv2.INTER_CUBIC
+
+    x_pos = (dif - w)//2
+    y_pos = (dif - h)//2
+
+    if len(img.shape) == 2:
+        mask = np.zeros((dif, dif), dtype=img.dtype)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w] = img[:h, :w]
+    else:
+        mask = np.zeros((dif, dif, c), dtype=img.dtype)
+        mask[y_pos:y_pos+h, x_pos:x_pos+w, :] = img[:h, :w, :]
+
+    return cv2.resize(mask, size, interpolation)
